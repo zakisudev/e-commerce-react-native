401faf2643e0c5e30000fc3ad68455134f0051c4
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-13 17:16:51 +0300

    Update carousel images

--- components/home/Carousel.jsx
+++ components/home/Carousel.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { SliderBox } from 'react-native-image-slider-box';
 import { COLORS } from '../../assets/constants';
+
 const Carousel = () => {
   const slides = [
-    'https://google.com/image1.png',
-    'https://google.com/image2.png',
-    'https://google.com/image3.png',
+    'https://www.thespruce.com/thmb/2DdYHKJEIbqh34JJmSvG7gGeG9w=/fit-in/1500x2230/filters:no_upscale():max_bytes(150000):strip_icc()/spr-june-couches-test-crate-barrel-lounge-julia-warren-002-83e9ee0b19e54661a2b6aca94fe0c1d7.jpeg',
+    'https://www.thespruce.com/thmb/2DdYHKJEIbqh34JJmSvG7gGeG9w=/fit-in/1500x2230/filters:no_upscale():max_bytes(150000):strip_icc()/spr-june-couches-test-crate-barrel-lounge-julia-warren-002-83e9ee0b19e54661a2b6aca94fe0c1d7.jpeg',
+    'https://www.thespruce.com/thmb/2DdYHKJEIbqh34JJmSvG7gGeG9w=/fit-in/1500x2230/filters:no_upscale():max_bytes(150000):strip_icc()/spr-june-couches-test-crate-barrel-lounge-julia-warren-002-83e9ee0b19e54661a2b6aca94fe0c1d7.jpeg',
   ];
 
   return (
