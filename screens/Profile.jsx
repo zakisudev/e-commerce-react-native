fdeb0a67d0ba67d826365da1138944b00e4205ad
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-13 17:14:42 +0300

    Update import statement in Profile.jsx

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
   return (
