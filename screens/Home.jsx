74063eb078bb61207c7980437d338d24d9da78d8
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-13 17:14:30 +0300

    Refactor UI components and add navigation

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,38 +1,50 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  TouchableOpacity,
+  ScrollView,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 import styles from './home.style';
 import { Ionicons } from '@expo/vector-icons';
 import Welcome from '../components/home/Welcome';
-import { ScrollView } from 'react-native-gesture-handler';
-import Carousel from 'react-native-snap-carousel';
+import Carousel from '../components/home/Carousel';
 import Headings from '../components/home/Headings';
 import ProductRow from '../components/products/ProductRow';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
-    <SafeAreaView>
-      <View style={styles.appBarHorizontal}>
-        <View style={styles.appBar}>
-          <Ionicons name="menu" size={24} color="black" />
-          <Text style={styles.location}>Addis Ababa</Text>
-          <Ionicons name="search" size={24} color="black" />
-          <View style={{ alignItems: 'flex-end' }}>
-            <View style={styles.cartCount}>
-              <Text style={styles.cartNumber}>3</Text>
+    <GestureHandlerRootView>
+      <SafeAreaView>
+        <View style={styles.appBarHorizontal}>
+          <View style={styles.appBar}>
+            <Ionicons name="menu" size={24} color="black" />
+            <Text style={styles.location}>Addis Ababa</Text>
+          </View>
+
+          <View style={styles.appBar}>
+            <Ionicons name="search" size={24} color="black" />
+            <View style={{ alignItems: 'flex-end' }}>
+              <View style={styles.cartCount}>
+                <Text style={styles.cartNumber}>3</Text>
+              </View>
+              <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
+                <Ionicons name="cart" size={24} color="black" />
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
-              <Ionicons name="cart" size={24} color="black" />
-            </TouchableOpacity>
           </View>
         </View>
-      </View>
-      <ScrollView>
-        <Welcome />
-        <Carousel />
-        <Headings />
-        <ProductRow />
-      </ScrollView>
-    </SafeAreaView>
+
+        <ScrollView>
+          <Welcome />
+          <Carousel />
+          <Headings />
+          <ProductRow />
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,32 +1,39 @@
 import { Text, TextInput, View } from 'react-native';
-import { SafeAreaView } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
 import { COLORS } from '../assets/constants';
 import styles from './search.style';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.searchContainer}>
-        <TouchableOpacity>
-          <Feather name="search" size={24} style={styles.searchIcon} />
-        </TouchableOpacity>
-        <View style={styles.searchWrapper}>
-          <TextInput
-            style={styles.searchInput}
-            value=""
-            onPressIn={() => {}}
-            placeholder="Search what you are looking for"
-          />
-        </View>
-        <View>
-          <TouchableOpacity style={styles.searchBtn}>
-            <Feather name="search" size={24} color={COLORS.offwhite} />
+    <GestureHandlerRootView>
+      <SafeAreaView>
+        <View style={styles.searchContainer}>
+          <TouchableOpacity>
+            <Feather name="search" size={24} style={styles.searchIcon} />
           </TouchableOpacity>
+
+          <View style={styles.searchWrapper}>
+            <TextInput
+              style={styles.searchInput}
+              value=""
+              onPressIn={() => {}}
+              placeholder="Search what you are looking for"
+            />
+          </View>
+
+          <View>
+            <TouchableOpacity style={styles.searchBtn}>
+              <Feather name="search" size={24} color={COLORS.offwhite} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
