da9c314aab710584deddab6b9cad5e018e111626
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-14 05:56:17 +0300

    Update imports and add gesture handler to Welcome component

--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,7 +1,11 @@
 import { View, Text, SafeAreaView } from 'react-native';
 import styles from './welcome.style';
 import { COLORS, SIZES } from '../../assets/constants';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { Feather, Ionicons } from '@expo/vector-icons';
 
@@ -9,39 +13,41 @@ const Welcome = () => {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text style={styles.welcomeTxt(COLORS.black, SIZES.xLarge)}>
-          Find your best
-        </Text>
-        <Text style={styles.welcomeTxt(COLORS.primary, 0)}>Product yet</Text>
-      </View>
-
-      <View style={styles.searchContainer}>
-        <TouchableOpacity>
-          <Feather name="search" size={24} style={styles.searchIcon} />
-        </TouchableOpacity>
-
-        <View style={styles.searchWrapper}>
-          <TextInput
-            style={styles.searchInput}
-            value=""
-            onPressIn={() => navigation.navigate('Search')}
-            placeholder="What you are looking for"
-          />
+    <GestureHandlerRootView>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text style={styles.welcomeTxt(COLORS.black, SIZES.xLarge)}>
+            Find your best
+          </Text>
+          <Text style={styles.welcomeTxt(COLORS.primary, 0)}>Product yet</Text>
         </View>
 
-        <View>
-          <TouchableOpacity style={styles.searchBtn}>
-            <Ionicons
-              name="camera-outline"
-              size={SIZES.xLarge}
-              color={COLORS.offwhite}
-            />
+        <View style={styles.searchContainer}>
+          <TouchableOpacity>
+            <Feather name="search" size={24} style={styles.searchIcon} />
           </TouchableOpacity>
+
+          <View style={styles.searchWrapper}>
+            <TextInput
+              style={styles.searchInput}
+              value=""
+              onPress={() => navigation.navigate('Search')}
+              placeholder="What you are looking for"
+            />
+          </View>
+
+          <View>
+            <TouchableOpacity style={styles.searchBtn}>
+              <Ionicons
+                name="camera-outline"
+                size={SIZES.xLarge}
+                color={COLORS.offwhite}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
