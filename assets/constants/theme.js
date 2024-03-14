c961c0254fd0137ca581b3109b17a36c13763734
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-14 05:57:10 +0300

    Add newRival screen

--- assets/constants/theme.js
+++ assets/constants/theme.js
@@ -1,23 +1,21 @@
-
-import { Dimensions } from 'react-native'
+import { Dimensions } from 'react-native';
 const { height, width } = Dimensions.get('window');
 
 const COLORS = {
-  primary: "#2A4D50",
-  secondary: "#DDF0FF",
-  tertiary: "#FF7754",
+  primary: '#2A4D50',
+  secondary: '#DDF0FF',
+  tertiary: '#FF7754',
 
-  gray: "#83829A",
-  gray2: "#C1C0C8",
+  gray: '#83829A',
+  gray2: '#C1C0C8',
 
-  offwhite: "#F3F4F8",
-  white: "#FFFFFF",
-  black: "#000000",
-  red: "#e81e4d",
-  green: " #00C135",
-  lightWhite: "#FAFAFC",
+  offwhite: '#F3F4F8',
+  white: '#FFFFFF',
+  black: '#000000',
+  red: '#e81e4d',
+  green: ' #00C135',
+  lightWhite: '#FAFAFC',
 };
-
 
 const SIZES = {
   xSmall: 10,
@@ -27,13 +25,12 @@ const SIZES = {
   xLarge: 24,
   xxLarge: 44,
   height,
-  width
+  width,
 };
-
 
 const SHADOWS = {
   small: {
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -43,7 +40,7 @@ const SHADOWS = {
     elevation: 2,
   },
   medium: {
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
@@ -54,5 +51,4 @@ const SHADOWS = {
   },
 };
 
-
-export { COLORS, SIZES , SHADOWS };
+export { COLORS, SIZES, SHADOWS };
