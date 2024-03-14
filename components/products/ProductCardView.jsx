cac12ce05646f8f893cb6e4452e637b8ab0c9a0b
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-14 05:56:04 +0300

    Add GestureHandlerRootView to ProductCardView

--- components/products/ProductCardView.jsx
+++ components/products/ProductCardView.jsx
@@ -4,39 +4,42 @@ import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS } from '../../assets/constants';
 import { useNavigation } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const ProductCardView = ({ item }) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('ProductDetails', { item })}
-    >
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-            source={{
-              uri: item.imageUrl,
-            }}
-            style={styles.image}
-          />
-        </View>
+    <GestureHandlerRootView>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('ProductDetails', { item })}
+      >
+        <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <Image
+              source={{
+                uri: item.imageUrl,
+              }}
+              style={styles.image}
+            />
+          </View>
 
-        <View style={styles.details}>
-          <Text style={styles.title} numberOfLines={1}>
-            {item.title}
-          </Text>
-          <Text style={styles.supplier} numberOfLines={1}>
-            {item.supplier}
-          </Text>
-          <Text style={styles.price}>${item.price}</Text>
-        </View>
+          <View style={styles.details}>
+            <Text style={styles.title} numberOfLines={1}>
+              {item.title}
+            </Text>
+            <Text style={styles.supplier} numberOfLines={1}>
+              {item.supplier}
+            </Text>
+            <Text style={styles.price}>${item.price}</Text>
+          </View>
 
-        <TouchableOpacity style={styles.addBtn}>
-          <Ionicons name="add-circle" size={35} color={COLORS.primary} />
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
+          <TouchableOpacity style={styles.addBtn}>
+            <Ionicons name="add-circle" size={35} color={COLORS.primary} />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    </GestureHandlerRootView>
   );
 };
 
