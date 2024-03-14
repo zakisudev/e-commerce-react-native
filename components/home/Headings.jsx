a9c88081df07a576fb4b8a11c0ca5d157be1191b
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-14 05:56:33 +0300

    Update Headings component to include navigation to ProductsList

--- components/home/Headings.jsx
+++ components/home/Headings.jsx
@@ -2,18 +2,26 @@ import { View, Text } from 'react-native';
 import styles from './headings.style';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS } from '../../assets/constants';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Headings = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <View style={styles.headers}>
-        <Text style={styles.headerTitle}>New Rivals</Text>
-        <TouchableOpacity>
-          <Ionicons name="grid" size={24} color={COLORS.primary} />
-        </TouchableOpacity>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <View style={styles.headers}>
+          <Text style={styles.headerTitle}>New Rivals</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('ProductsList')}>
+            <Ionicons name="grid" size={24} color={COLORS.primary} />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
--- components/home/headings.style.js
+++ components/home/headings.style.js
@@ -1,4 +1,4 @@
-import { COLORS, SIZES } from '../../assets/constants';
+import { SIZES } from '../../assets/constants';
 
 const styles = {
   container: {
